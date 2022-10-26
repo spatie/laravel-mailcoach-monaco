8652e0b89ae8dc8513c59c472b8f9b730c42c0e7
riasvdv <rias@spatie.be>
2022-10-26 11:40:56 +0000

    formatting

--- src/MailcoachMonacoServiceProvider.php
+++ src/MailcoachMonacoServiceProvider.php
@@ -3,21 +3,25 @@
 namespace Spatie\MailcoachMonaco;
 
 use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
+use Spatie\Mailcoach\Mailcoach;
 
 class MailcoachMonacoServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->mergeConfigFrom(__DIR__ . '/../config/mailcoach-monaco.php', 'mailcoach-monaco');
+
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'mailcoach-monaco');
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__.'/../resources/views' => base_path('resources/views/vendor/mailcoach/monaco'),
             ], 'mailcoach-monaco-views');
-
-            $this->publishes([
-                __DIR__.'/../resources/monaco' => public_path('vendor/mailcoach/monaco/vs'),
-            ], 'mailcoach-monaco-assets');
         }
+
+        Livewire::component('mailcoach-monaco::editor', MonacoEditor::class);
+
+        Mailcoach::editorScript(MonacoEditor::class, 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.21.2/min/vs/loader.min.js');
     }
 }
