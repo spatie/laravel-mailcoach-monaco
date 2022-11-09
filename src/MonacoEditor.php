99c4f818932d5dd591f6dc4a9c688c274edcefcb
riasvdv <rias@spatie.be>
2022-11-09 10:55:12 +0000

    Add v5 to branches

--- src/MonacoEditor.php
+++ src/MonacoEditor.php
@@ -2,13 +2,18 @@
 
 namespace Spatie\MailcoachMonaco;
 
-use Illuminate\Contracts\View\View;
-use Spatie\Mailcoach\Http\App\Livewire\EditorComponent;
+use Spatie\Mailcoach\Domain\Campaign\Models\Concerns\HasHtmlContent;
+use Spatie\Mailcoach\Domain\Campaign\Models\Template;
+use Spatie\Mailcoach\Domain\Shared\Support\Editor\Editor;
+use Spatie\Mailcoach\Domain\TransactionalMail\Models\TransactionalMailTemplate;
 
-class MonacoEditor extends EditorComponent
+class MonacoEditor implements Editor
 {
-    public function render(): View
+    public function render(HasHtmlContent $model): string
     {
-        return view('mailcoach-monaco::editor');
+        return view('mailcoach-monaco::editor', [
+            'html' => $model->getHtml(),
+            'showTestButton' => ! $model instanceof Template && ! $model instanceof TransactionalMailTemplate,
+        ])->render();
     }
 }
